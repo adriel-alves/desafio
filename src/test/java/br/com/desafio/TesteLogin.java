345c20f0af632b8dde0110cfa7cfda7211859689
AdrielFreitas <adrielfreitasalves@gmail.com>
2017-10-01 09:16:44 -0300

    desafio concrete

---
+++ src/test/java/br/com/desafio/TesteLogin.java
@@ -0,0 +1,21 @@
+package br.com.desafio;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TesteLogin {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
